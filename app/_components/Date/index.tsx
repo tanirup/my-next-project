6454cda2878ccecd81317474e5922f8201b6f950
aoto0218 <ktc24a31g0010@edu.kyoto-tech.ac.jp>
2024-11-30 16:30:20 +0900

    編集

---
+++ app/_components/Date/index.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import style from "./index.module.css";
+
+type Props = {
+    date: string;
+};
+
+export default function Date({ date }: Props) {
+    return (
+        <span className= {style.date}>
+          <Image src="/clock.svg" alt="" width={16} height={16}
+loading="eager"  />
+        {date}
+       </span>
+    );
+}
